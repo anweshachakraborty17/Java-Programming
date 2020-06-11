b7aefd596cb1bcf8a46cf4acc236eb9420905cdf
Anwesha Chakraborty <anweshachakraborty17@gmail.com>
2020-06-11 23:55:46 +0530

    Add files via upload

---
+++ Java Loops-I Answer.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    for(int i=1; i<11; i++)
+    {
+        System.out.println(N+" x "+i+" = "+N*i);
+    }
+}
+
+}
